a0435f61a8f158afddf0615eacc000f026600042
Shubham Kori <shubhamkori23@gmail.com>
2022-06-27 14:26:31 +0530

    Created and Connected MongoDB using Mongoose Package

--- To-Do App/controllers/todoController.js
+++ To-Do App/controllers/todoController.js
@@ -1,11 +1,27 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'Get Dairy'}, {item: 'Hit the Gym'}, {item: 'Do some Coding'}];
-var urlencodedParser = bodyParser.urlencoded({extended: false}); //middleware
+//Connect To The Database
+mongoose.connect('mongodb+srv://Shubh:6668@cluster0.tt98tdj.mongodb.net/todo');
+
+//Create a Schema - This is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+//Create a Model
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({ item: 'Buy Flowers' }).save(function (err) {
+    if (err) throw err;
+    console.log('Item Saved');
+})
+
+var data = [{ item: 'Get Dairy' }, { item: 'Hit the Gym' }, { item: 'Do some Coding' }];
+var urlencodedParser = bodyParser.urlencoded({ extended: false }); //middleware
 
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render('todo', {todos: data});
+        res.render('todo', { todos: data });
     });
 
     app.post('/todo', urlencodedParser, function (req, res) {
@@ -14,7 +30,7 @@ module.exports = function (app) {
     });
 
     app.delete('/todo/:item', function (req, res) {
-        data = data.filter(function(todo) {
+        data = data.filter(function (todo) {
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
         res.json(data);
