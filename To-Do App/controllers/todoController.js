857b7ce8582add16453fd168ca02014d2e73df21
Shubham Kori <shubhamkori23@gmail.com>
2022-06-27 09:40:35 +0530

    Update todoController.js

--- To-Do App/controllers/todoController.js
+++ To-Do App/controllers/todoController.js
@@ -1,3 +1,11 @@
-module.exports = function(app){
-    
-}
+module.exports = function (app) {
+    app.get('/todo', function (req, res) {
+        res.render('todo');
+    });
+
+    app.post('/todo', function (req, res) {
+    });
+
+    app.delete('/todo', function (req, res) {
+    });
+};
