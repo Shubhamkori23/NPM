162c3800b1b4fe7660df37efd082a960af9e7036
Shubham Kori <shubhamkori23@gmail.com>
2022-06-27 11:51:03 +0530

    Update todoController.js

--- To-Do App/controllers/todoController.js
+++ To-Do App/controllers/todoController.js
@@ -1,9 +1,16 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'Get Dairy'}, {item: 'Hit the Gym'}, {item: 'Do some Coding'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false}); //middleware
+
 module.exports = function (app) {
     app.get('/todo', function (req, res) {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function (req, res) {
+    app.post('/todo', urlencodedParser, function (req, res) {
+        data.push(req.body);
+        res.json({todos: data});
     });
 
     app.delete('/todo', function (req, res) {
