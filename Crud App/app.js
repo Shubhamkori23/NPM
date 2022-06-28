f1c185244ba915328beb82c9a28d5509f94f578c
Shubham Kori <shubhamkori23@gmail.com>
2022-06-28 09:17:49 +0530

    Update app.js

--- Crud App/app.js
+++ Crud App/app.js
@@ -3,64 +3,65 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Book = require('./Book.model');
+const { functionsIn } = require('lodash');
 
 var port = 8080;
 mongoose.connect('mongodb+srv://<username>:<password>@cluster0.tt98tdj.mongodb.net/example');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Happy to be here');
 });
 
 //get all books from mongodb
-app.get('/books', function(req, res){
+app.get('/books', function (req, res) {
   console.log('getting all books');
   Book.find({})
-  .exec(function(err, books){
-      if(err){
-          res.send('error has occured');
-      } else{
-          console.log(books);
-          res.json(books);
+    .exec(function (err, books) {
+      if (err) {
+        res.send('error has occured');
+      } else {
+        console.log(books);
+        res.json(books);
       }
-  })
+    })
 });
 
 //get books by id from the database
-app.get('/books/:id', function(req,res){
+app.get('/books/:id', function (req, res) {
   console.log('getting one book');
   Book.findOne({
-      _id:req.params.id
+    _id: req.params.id
   })
-  .exec(function(err, book){
-      if(err){
-          res.send('error occured');
-      }else{
-          console.log(book);
-          res.json(book);
+    .exec(function (err, book) {
+      if (err) {
+        res.send('error occured');
+      } else {
+        console.log(book);
+        res.json(book);
       }
-  })
+    })
 });
 
 //add a book in the database
-app.post('/book', function(req, res){
+app.post('/book', function (req, res) {
   var newBook = new Book();
 
-  newBook.title=req.body.title;
-  newBook.author=req.body.author;
-  newBook.category=req.body.category;
+  newBook.title = req.body.title;
+  newBook.author = req.body.author;
+  newBook.category = req.body.category;
 
-  newBook.save(function(err, book){
-      if(err){
-          res.send('errors occured');
-      }else{
-          console.log(book);
-          res.send(book);
-      }
+  newBook.save(function (err, book) {
+    if (err) {
+      res.send('errors occured');
+    } else {
+      console.log(book);
+      res.send(book);
+    }
   });
 })
 
@@ -76,7 +77,22 @@ app.post('/book', function(req, res){
 //     });
 //   });
 
+app.put('/book/:id', function (req, res) {
+  Book.findOneAndUpdate({
+    _id: req.params.id
+  },
+    {
+      $set: { title: req.body.title }
+    }, { upsert: true }, function (err, newBook) {
+      if (err) {
+        res.send('error updating ');
+      } else {
+        console.log(newBook);
+        res.send(newBook);
+      }
+    });
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('app listening on port ' + port);
 });
