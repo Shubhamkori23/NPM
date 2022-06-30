ae53ddf8c2d9993f97595af4a66e2ad80e8276a3
Shubham Kori <shubhamkori23@gmail.com>
2022-06-30 17:59:31 +0530

    Update server.js

--- Login Application with Node & Express/server.js
+++ Login Application with Node & Express/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var path = require("path");
 var app = express();
 
 var port = process.env.PORT||3000;
 
 app.set('view engine', 'ejs');
 
+//load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 //home route
 app.get('/', (req, res) =>{
     res.render('base', { title : "Login System"});
