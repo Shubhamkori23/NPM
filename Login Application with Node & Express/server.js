19ee30c5dda7860bb62aac75feabd7a3e94f4b57
Shubham Kori <shubhamkori23@gmail.com>
2022-06-30 18:18:05 +0530

    Update server.js

--- Login Application with Node & Express/server.js
+++ Login Application with Node & Express/server.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var path = require("path");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var { v4: uuidv4 } = require("uuid");
+
 var app = express();
 
-var port = process.env.PORT||3000;
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.set('view engine', 'ejs');
 
 //load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+app.use(session({
+    secret: uuidv4(), //  '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+    resave: false,
+    saveUninitialized: true
+}))
+
 //home route
-app.get('/', (req, res) =>{
-    res.render('base', { title : "Login System"});
+app.get('/', (req, res) => {
+    res.render('base', { title: "Login System" });
 })
 
-app.listen(port, ()=>{console.log('Listening to the server on http://localhost:3000')})
+app.listen(port, () => { console.log('Listening to the server on http://localhost:3000') })
