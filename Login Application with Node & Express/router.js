371806fb608568c00f2140deb299beb3703ee07c
Shubham Kori <shubhamkori23@gmail.com>
2022-07-01 08:51:08 +0530

    Create router.js

---
+++ Login Application with Node & Express/router.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+var  credential = {
+    email : "admin@gmail.com",
+    password : "admin123"
+}
+
+// login user
+router.post('/login', (req, res)=>{
+    if(req.body.email == credential.email && req.body.password == credential.password){
+        req.session.user = req.body.email;
+        res.redirect('/route/dashboard');
+        res.end("Login Successful...!");
+    }else{
+        res.end("Invalid Username/Password")
+    }
+});
